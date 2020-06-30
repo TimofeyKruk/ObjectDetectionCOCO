15c13356b9fab31fb0724e4c666b75411ca99fb7
Kruk Timofey <kruktimofey@gmail.com>
2020-06-30 11:13:15 +0300

    README changed

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-#Object Detection COCO dataset  
+# Object Detection COCO dataset  
 
 Will add details, results and structure later. Sorry for inconvinience! Work in progress
+
+### Performance
+
+#### Contacts:
+Created by Timofey Kruk
+kruktimofey@gmail.com
--- customTransform.py
+++ customTransform.py
@@ -1,4 +1,13 @@
 import torch
 import torchvision
+import cv2
 
-class
+class Resize():
+    def __init__(self,img_size) -> None:
+        super().__init__()
+        self.img_size=img_size
+
+    def __call__(self, images, targets):
+        print("Resize().__call__ was called!!!!!!!@!@!@!")
+
+
